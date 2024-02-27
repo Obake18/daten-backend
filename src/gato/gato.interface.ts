3ef2b9bc879b0e55a969a83659fa03bb8c80295b
Obake18 <coimbrajuniorm@gmail.com>
2024-02-27 16:48:47 -0300

    fff

---
+++ src/gato/gato.interface.ts
@@ -0,0 +1,8 @@
+export interface Gato {
+
+    id: number;
+    nome : string;
+    raca : string;
+    idade : number;
+
+}
--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Gato } from 'src/gato/gato.interface';
 
 @Controller('gatos')
-export class GatosController {}
+export class GatosController {
+    private readonly gatos: Gato[] = [
+        {
+            id: 1,
+            nome: 'Black',
+            raca: 'mestiça',
+            idade: 2,
+
+        },
+
+        {
+            id: 2,
+            nome: 'Arthur',
+            raca:'Pura',
+            idade: 2,
+        },
+
+        {
+            id: 3,
+            nome: 'Apollo',
+            raca: 'Mauris',
+            idade: 9,
+        }
+    ];
+
+
+    @Get()
+    findAll(): Gato[] {
+        return this.gatos;
+    }
+    
+    @Get(':id')
+
+    findOne(@Param('id') id: number): Gato{
+        console.log(id);
+        return {
+            id: 3,
+            nome: 'Apollo',
+            raca: 'Mauris',
+            idade: 9,
+        };
+    }
+}
+
