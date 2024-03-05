0c16511a1febb4bb3e4c513ad2a77a90aa849368
Obake18 <coimbrajuniorm@gmail.com>
2024-03-05 16:35:38 -0300

    colocando DELETE E UPDATE

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { create } from 'domain';
+import { Response } from 'express';
 import { Gato } from 'src/gato/gato.interface';
 
 @Controller('gatos')
@@ -9,16 +11,13 @@ export class GatosController {
             nome: 'Black',
             raca: 'mestiça',
             idade: 2,
-
         },
-
         {
             id: 2,
             nome: 'Arthur',
             raca:'Pura',
             idade: 2,
         },
-
         {
             id: 3,
             nome: 'Apollo',
@@ -27,15 +26,15 @@ export class GatosController {
         }
     ];
 
-
+    // Retorna todos os gatos
     @Get()
     findAll(): Gato[] {
         return this.gatos;
     }
-    
-    @Get(':id')
 
-    findOne(@Param('id') id: number): Gato{
+    // Retorna um gato específico pelo id
+    @Get(':id')
+    findOne(@Param('id') id: number): Gato {
         console.log(id);
         return {
             id: 3,
@@ -44,5 +43,32 @@ export class GatosController {
             idade: 9,
         };
     }
-}
 
+    // Exclui um gato pelo id
+    @Delete(':id')
+    delete(@Param('id') id: string, @Res() response: Response) {
+        const index = this.gatos.findIndex((gato) => gato.id === Number(id));
+        if (index >= 0) {
+            this.gatos.splice(index, 1);
+            response.status(HttpStatus.OK).send('Gato removido com sucesso, SEU ASSASSINO!! QUERO TODO O SOFRIMENTO NA SUA VIDA, QUE A DESGRAÇA CAIA NAS SUAS COSTAS!!');
+        } else {
+            response.status(HttpStatus.NOT_FOUND).send('Não foi possível encontrar o gato');
+        }
+    }
+    
+    // Cria um novo gato
+    @Post()
+    create(@Body() gato : Gato) {
+        this.gatos.push(gato);
+        return gato;
+    }
+
+    @Put(':id')
+    update(@Body() gato : Gato) {
+        const index = this.gatos.findIndex((gato) => gato.id === Number(gato.id));
+        if (index >= 0) {
+            this.gatos[index] = gato;
+        }
+        return gato;
+    };
+}
