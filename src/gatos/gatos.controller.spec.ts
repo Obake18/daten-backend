81e4d638ab7f61c2b3e3d9b2d10f7297e88db512
Obake18 <coimbrajuniorm@gmail.com>
2024-03-05 13:53:56 -0300

    Documentação feita.

--- src/gatos/gatos.controller.spec.ts
+++ src/gatos/gatos.controller.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GatosController } from './gatos.controller';
 
+// Este código está testando a classe GatosController
+
 describe('GatosController', () => {
   let controller: GatosController;
 
+  // Configura o módulo de teste antes de cada caso de teste
+
   beforeEach(async () => {
+    // Cria um módulo de teste usando o método createTestingModule
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GatosController],
     }).compile();
 
+    // Obtém uma instância da classe GatosController do módulo de teste
     controller = module.get<GatosController>(GatosController);
   });
 
+  // Caso de teste: verifica se o controller está definido
+
   it('should be defined', () => {
+    // Espera-se que o objeto controller esteja definido
     expect(controller).toBeDefined();
   });
 });
